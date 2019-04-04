639162943296784edfb85f7a94d11c32c923e1cb
keer2345 <keer2345@gmail.com>
2019-04-04 22:31:54 +0000

    ch01

---
+++ tacos/src/test/java/com/keer/tacos/web/controller/HomePageBrowserTest.java
@@ -0,0 +1,48 @@
+package com.keer.tacos.web.controller;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HomePageBrowserTest {
+    @LocalServerPort
+    private int port;
+    private static HtmlUnitDriver browser;
+
+    @BeforeClass
+    public static void setup() {
+        browser = new HtmlUnitDriver();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void teardown() {
+        browser.quit();
+    }
+
+    @Test
+    public void testHomePage() {
+        String homepage = "http://localhost:" + port;
+        browser.get(homepage);
+        String titleText = browser.getTitle();
+        Assert.assertEquals("Taco Cloud", titleText);
+
+        String h1Text = browser.findElementByTagName("h1").getText();
+        Assert.assertEquals("Welcome to ...", h1Text);
+
+        String imgSrc = browser.findElementByTagName("img").getAttribute("src");
+        System.out.println(">>>>>>><<<<<<<<========");
+        System.out.println(imgSrc);
+        Assert.assertEquals(homepage + "/images/TacoCloud.png", imgSrc);
+    }
+}
