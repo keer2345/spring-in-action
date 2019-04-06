2da020bb84090f809caf2704ca1be1f9b1b703c1
keer2345 <keer2345@gmail.com>
2019-04-06 09:35:33 +0000

    OrderController

---
+++ tacos/src/main/java/com/keer/tacos/web/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.keer.tacos.web.controller;
+
+import com.keer.tacos.web.entity.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+}
