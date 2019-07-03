5343ef51f35f8426f61c051c6ce5b783f53075b5
keer2345 <keer2345@gmail.com>
2019-07-03 13:48:36 +0800

    spring-boot-starter-data-rest

--- code/ch06/tacos/src/main/java/com/tacos/entity/Taco.java
+++ code/ch06/tacos/src/main/java/com/tacos/entity/Taco.java
@@ -1,6 +1,7 @@
 package com.tacos.entity;
 
 import lombok.Data;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Data
 @Entity
-//todo
+@RestResource(rel = "tacos", path = "tacos")
 public class Taco {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
